cc613c4f59c8ddcabf434f427db15d9c6c606ab0
farfromsouls <farfromsouls@gmail.com>
2025-07-22 23:28:35 +0700

    nginx preparing

--- start.bat
+++ start.bat
@@ -1,14 +1,23 @@
 @echo off
+setlocal
 
+rem checking modules
 if not exist "%~dp0frontend/node_modules" (
     start "NodeModulesInstall" cmd /k "cd /d %~dp0frontend && npm i"
 )
 
 if not exist "%~dp0backend/venv" (
-    start "PythonModulesInstall" cmd /k "cd /d %~dp0backend && venv\Scripts\activate && pip install -r requirements.txt"
+    start "PythonModulesInstall" cmd /k "cd /d %~dp0backend && python -m venv venv && venv\Scripts\activate && pip install -r requirements.txt"
 )
 
-start "Backend" cmd /k "cd /d %~dp0backend && venv\Scripts\activate && python manage.py runserver"
-start "Frontend" cmd /k "cd /d %~dp0frontend && npm start"
+rem starting backend
+start "Backend" cmd /k "cd /d %~dp0backend && venv\Scripts\activate && python manage.py runserver && exit"
+
+rem starting frontend
+start "Frontend" cmd /k "cd /d %~dp0frontend && npm start && exit"
+
+rem opening API page
+timeout /t 5 >nul
+start "" "http://127.0.0.1:8000/"
 
 exit
